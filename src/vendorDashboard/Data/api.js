df0a908812cbdccaeb0cbeb77699cda1ac722191
Vishnu <uyyalavishnubharadwaj@gmail.com>
2025-06-12 11:58:30 +0530

    changed path to live in api_url

--- src/vendorDashboard/Data/api.js
+++ src/vendorDashboard/Data/api.js
@@ -1,4 +1,4 @@
 // export const API_URL="http://localhost:4000"
-export const API_URL = "http://localhost:4000";  // ✅ Ensure this is correct
+// export const API_URL = "http://localhost:4000";  // ✅ Ensure this is correct
 
-// const API_URL="https://backend-nodejs-1nt5.onrender.com"
+const API_URL="https://backend-nodejs-1nt5.onrender.com"
